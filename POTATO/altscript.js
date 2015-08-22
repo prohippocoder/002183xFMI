3a595c969675ab394f9b116b3ed848b545a1ca4c
prohippocoder <prohippocoder@gmail.com>
2015-08-22 11:57:27 +0800

    Create altscript.js

---
+++ POTATO/altscript.js
@@ -0,0 +1,27 @@
+var start (Start)
+
+	return ();
+
+var alt1 (ProductCodes01)
+
+	createcommand_alt (productcodes1)
+		alt="codes1"
+		output "productcodes_01.csv"
+
+	return ();
+	
+var alt2 (productcodes2)
+	
+	createcommand_alt (productcodes2)
+		alt="codes2"
+		output="productcodes_02.csv"
+		
+	return ();
+
+var alt3 (Persentage)
+	
+	createcommand_alt (persentage)
+		alt="persentage"
+		output="%"
+		
+	return ();
